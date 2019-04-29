871a26444118aded4c96f0fb519eca9115cda86b
chalice <chalice@unnotech.com>
2019-04-29 11:28:22 +0800

    f

--- stacks/deploy_stacks.sh
+++ stacks/deploy_stacks.sh
@@ -1,4 +1,6 @@
-docker stack deploy -c stack-panther-master-db.yml --with-registry-auth z-staging
-docker stack deploy -c stack-message-broker.yml --with-registry-auth z-staging
-docker stack deploy -c stack-panther.yml --with-registry-auth z-staging
-docker stack deploy -c stack-panther-load-balancer.yml --with-registry-auth z-staging
+docker stack deploy -c stack-demo-master-db.yml --with-registry-auth chalice_ser
+docker stack deploy -c stack-message-broker.yml --with-registry-auth chalice_ser
+docker stack deploy -c stack-demo.yml --with-registry-auth chalice_ser
+docker stack deploy -c stack-demo-load-balancer.yml --with-registry-auth chalice_ser
+docker stack deploy -c stack-auto-deploy.yml --with-registry-auth chalice_ser
+
